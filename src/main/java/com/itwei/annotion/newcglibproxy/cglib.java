12bf79481a53333c99bcaf6a3c90258ef04ccecd
v_mjwei <v_mjwei@v_mjwei-PC1.tencent.com>
2019-09-24 14:31:43 +0800

    cglib 提交

---
+++ src/main/java/com/itwei/annotion/newcglibproxy/cglib.java
@@ -0,0 +1,35 @@
+package com.itwei.annotion.newcglibproxy;
+
+import com.itwei.annotion.jdkproxy.Host;
+import com.itwei.annotion.staticproxy.XiaoMing;
+import com.sun.xml.internal.bind.v2.runtime.output.Encoded;
+import com.sun.xml.internal.ws.message.source.PayloadSourceMessage;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class cglib implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+
+        System.out.println("i am zhong jie see you shopping");
+        Object o1 = methodProxy.invokeSuper(o, objects);
+        System.out.println("i am zhong jie see you shopping gitover");
+        return o1;
+    }
+}
+class Test01 {
+
+    //又类转换异常啦 不知道为啥
+    public static void main(String[] args) {
+        cglib cglib = new cglib();
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(XiaoMing.class);
+        enhancer.setCallback(cglib);
+        Host h = (Host) enhancer.create();
+        h.mia();
+    }
+}
