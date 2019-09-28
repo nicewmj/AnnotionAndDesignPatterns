592f94349c729b58d1c22745b8559b125c1a1a21
wei <1729148544@qq.com>
2019-09-29 00:02:19 +0800

    sql 优化 取模分表

---
+++ src/main/java/sql/UserController.java
@@ -0,0 +1,35 @@
+package sql;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/regit")
+    @ResponseBody
+    public String  regit(@RequestParam String name , @RequestParam String pwd){
+        return userService.regit(name,pwd);
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public String get(@RequestParam Long id){
+        String name = userService.get(id);
+        return name;
+
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+    return "你好";
+    }
+}
---
+++ src/main/java/sql/UserService.java
@@ -0,0 +1,49 @@
+package sql;
+
+import com.sun.javafx.property.JavaBeanAccessHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    //引入模板JdbcTemplate
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public String regit(String name, String pwd) {
+
+        // 1.先获取到 自定增长ID
+        String idIndexSQL = "INSERT INTO UUID VALUE (NULL)";
+
+        int update = jdbcTemplate.update(idIndexSQL);
+        Long insertId = jdbcTemplate.queryForObject("select last_insert_id()", Long.class);
+
+        // 2.判断存储表名称
+        String tableName = "user" + insertId % 3;
+
+        // 3.注册数据  "INSERT INTO user VALUES (name,pwd)";
+        String insertUserSql = "INSERT INTO " + tableName + " VALUES ('" + insertId + "','" + name + "','" + pwd+ "');";
+
+        System.out.println("insertUserSql  " + insertUserSql);
+
+        jdbcTemplate.update(insertUserSql);
+
+        return "seccess";
+
+    }
+
+    public String get(Long id){
+
+        String tableName = "user"+id % 3;
+
+        String sql ="select name  from " + tableName +"  where id="+id;
+
+        System.out.println("SQL "+sql);
+
+        String name = jdbcTemplate.queryForObject(sql,String.class);
+
+        return name;
+    }
+}
